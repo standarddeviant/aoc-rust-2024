2065a27454e9edcea8facabf1f67c092e0cea8ff
standarddeviant <dave.crist@gmail.com>
2024-12-04 08:41:57 -0500

    day04, both parts correct

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -72,6 +72,47 @@ fn count_2d_str(needle: Vec<char>, haystack: Vec<Vec<char>>) -> usize {
     count
 }
 
+fn count_2d_x_mas(haystack: Vec<Vec<char>>) -> usize {
+    let mut count = 0;
+    let ndl = vec!['M', 'A', 'S'];
+    let hay = haystack;
+    let nrows = hay.len();
+    assert!(nrows > 0);
+    let ncols = hay[0].len();
+
+    for rix in 0..nrows {
+        for cix in 0..ncols {
+            if 'A' != hay[rix][cix] {
+                continue;
+            }
+            let check_rc = (0 < rix && rix < nrows - 1) && (0 < cix && cix < ncols - 1);
+            if !check_rc {
+                continue;
+            }
+
+            // the string-reversing is redundant since we know A is in the middle, but it's
+            // functional. A more optimal approach would be to hide messy if/else logic in a
+            // helper function
+            let check_dr_1 = vec![hay[rix - 1][cix - 1], hay[rix][cix], hay[rix + 1][cix + 1]];
+            let mut check_dr_2 = check_dr_1.clone();
+            check_dr_2.reverse();
+
+            let check_ur_1 = vec![hay[rix + 1][cix - 1], hay[rix][cix], hay[rix - 1][cix + 1]];
+            let mut check_ur_2 = check_ur_1.clone();
+            check_ur_2.reverse();
+
+            let check_ur = ndl.eq(&check_ur_1) || ndl.eq(&check_ur_2);
+            let check_dr = ndl.eq(&check_dr_1) || ndl.eq(&check_dr_2);
+
+            if check_dr && check_ur {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
 fn main() {
     let args = Args::parse();
     let haystack = parse_2d_chars(args.input.clone());
@@ -79,7 +120,10 @@ fn main() {
     // println!("haystack = {haystack:?}");
 
     let count1 = count_2d_str(vec!['X', 'M', 'A', 'S'], haystack.clone());
-    println!("count1 = {count1:?}");
+    println!("day04, part1 = {count1:?}");
+
+    let count2 = count_2d_x_mas(haystack.clone());
+    println!("day04, part2 = {count2:?}");
 }
 
 // EOF
