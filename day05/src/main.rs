1622574e6ce9f0b0193bcc2b3640c3df99c08020
standarddeviant <dave.crist@gmail.com>
2024-12-05 07:34:03 -0500

    day05, both parts correct

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -32,41 +32,73 @@ fn parse_input(input: String) -> (Vec<Vec<i32>>, Vec<Vec<i32>>) {
     (rules, updates)
 }
 
-fn apply_rule(mut update: Vec<i32>, rule: Vec<i32>) -> Vec<i32> {
+fn apply_rule(update: &mut Vec<i32>, rule: Vec<i32>) {
     if update.contains(&rule[0]) && update.contains(&rule[1]) {
         loop {
-            let ixa = update.iter().position(|&x| x == rule[0]).unwrap();
-            let ixb = update.iter().position(|&x| x == rule[1]).unwrap();
-            if ixa < ixb {
+            let i0 = update.iter().position(|&x| x == rule[0]).unwrap();
+            let i1 = update.iter().position(|&x| x == rule[1]).unwrap();
+            if i0 < i1 {
+                // println!("stopping {rule:?} to {update:?}");
+                // println!(
+                //     "stopping to apply {rule:?} : ({} @ {i0}) -> ({} @ {i1})",
+                //     rule[0], rule[1]
+                // );
                 break;
             }
-            assert!(ixb < update.len() - 1);
-            update.swap(ixb, ixb + 1);
+            // println!("applying {rule:?} to {update:?}");
+            assert!(i1 < update.len() - 1);
+            update.swap(i1, i1 + 1);
         }
         // hmmm
     }
-    update
+}
+
+fn rules_correctly_applied(update: &mut Vec<i32>, rules: Vec<Vec<i32>>) -> bool {
+    for rule in rules {
+        if update.contains(&rule[0]) && update.contains(&rule[1]) {
+            let i0 = update.iter().position(|&x| x == rule[0]).unwrap();
+            let i1 = update.iter().position(|&x| x == rule[1]).unwrap();
+            if i1 < i0 {
+                // println!("rule {rule:?}, does not apply to {update:?}");
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 fn main() {
     let args = Args::parse();
     let (rules, updates) = parse_input(args.input);
-    // println!("rules = {:?}", rules.clone());
+    // println!("rules = {:#?}", rules.clone());
     // println!("updates = {updates:?}");
 
     let mut sum1 = 0;
+    let mut sum2 = 0;
     for u in &updates {
         let mut w = u.clone();
-        for rule in rules.clone() {
-            w = apply_rule(w, rule);
+        // at first it seemed only one pass of each rule was sufficient, but...
+        // the same number is handled by multiple rules
+        // so, what can happen is...
+        // a rule containing X,Y is applied
+        // then a rule about Z,X is applied
+        // with the algo I chose, the second operation can move X 'past' Y when applying Z
+        // So that's why we have a while loop with this simple algo
+        while !rules_correctly_applied(&mut w, rules.clone()) {
+            for rule in rules.clone() {
+                apply_rule(&mut w, rule);
+            }
         }
         if w.eq(u) {
-            // correct order, yay
-            // println!("correct order: {w:?}");
             sum1 += w[w.len() / 2];
+        } else {
+            // println!("w = {w:?}");
+            // println!("u = {u:?}");
+            sum2 += w[w.len() / 2];
         }
     }
     println!("day05, part1 = {sum1}");
+    println!("day05, part2 = {sum2}");
 }
 
 // EOF
