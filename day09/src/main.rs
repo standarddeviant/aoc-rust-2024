5a25ffdcd51e8fc8e8b29892e5bf3f4a362182b7
standarddeviant <dave.crist@gmail.com>
2024-12-10 07:31:41 -0500

    cleaned up day09, part1

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -14,69 +14,100 @@ struct Args {
 
 pub const SPACE: i64 = -1_i64;
 
-fn last_fill(v: &Vec<i64>) -> usize {
-    v.iter().positions(|x| *x >= 0).max().unwrap()
-}
-
-fn first_space(v: &Vec<i64>) -> usize {
-    v.iter().positions(|x| *x < 0).min().unwrap()
+fn printv(v: &Vec<i64>) {
+    let mut tmps: String = "".into();
+    let tmps = v
+        .iter()
+        .map(|i| {
+            if *i >= 0 {
+                format!("{i}")
+            } else {
+                format!(".")
+            }
+        })
+        .join("");
+    println!("{tmps}");
 }
 
 fn custom_sort_1(v: &mut Vec<i64>) {
-    let mut itr = 0;
+    let mut f = v.len() - 1;
+    let mut s = 0;
+    // let mut itr = 0;
     loop {
-        let _lastf = last_fill(v);
-        let _firsts = first_space(v);
-        if _lastf < _firsts {
+        // move s forward to next space
+        while v[s] >= 0 && s < v.len() - 1 {
+            s += 1;
+        }
+        // move f backward to next fill
+        while v[f] < 0 && f > 0 {
+            f -= 1;
+        }
+        // check if we should bail, we bail when...
+        // 1. space isnt space
+        // 2. fill isnt fill
+        // 3. fill < space
+        if v[s] >= 0 || v[f] < 0 || f < s {
             break;
         }
-        v.swap(_lastf, _firsts);
-        // println!("cs1: {itr} : {v:?}");
-        itr += 1;
+        // simple swap!
+        v.swap(s, f);
+
+        // remove + re-insert
+        // let fval = v.remove(f);
+        // v.insert(s, fval);
+
+        // printv(&v);
+        // itr += 1;
     }
 }
 
-fn parse_blocks(input: String) -> Vec<i64> {
-    let mut w: Vec<i64> = vec![];
+fn parse_fill_space(input: String) -> Vec<i64> {
+    let mut fs: Vec<i64> = vec![];
     // let re = Regex::new(r"\d+").unwrap();
 
     let contents = std::fs::read_to_string(input.clone())
         .expect(format!("Unable to read file ->{}<-", input.clone()).as_str());
     for line in contents.lines() {
         // there should only be one line...
-        let tmpv: Vec<String> = line.chars().map(|c| format!("{c}")).collect();
-        // println!("tmpv = {tmpv:?}");
-        // println!("tmpv.len() = {}", tmpv.len());
-        w = tmpv.iter().map(|s| s.parse::<i64>().unwrap()).collect();
+        let tmps: Vec<String> = line.chars().map(|c| format!("{c}")).collect();
+        // println!("fs = {}", tmpv.len());
+        fs = tmps.iter().map(|s| s.parse::<i64>().unwrap()).collect();
+        break;
     }
 
+    fs
+}
+
+fn expand_blocks(fs: Vec<i64>) -> Vec<i64> {
+    let mut blocks: Vec<i64> = vec![];
     // expand 'parsed-repr' into 'unsorted-block-repr'
-    let mut w2: Vec<i64> = vec![];
     let mut file_ix = 0;
-    for ix in 0..w.len() {
+    for ix in 0..fs.len() {
         let is_file = 0 == ix % 2;
         if is_file {
-            let tmp: Vec<i64> = vec![file_ix; w[ix] as usize];
-            w2.extend(tmp);
+            let tmp: Vec<i64> = vec![file_ix; fs[ix] as usize];
+            blocks.extend(tmp);
             file_ix += 1;
         } else {
-            let tmp: Vec<i64> = vec![-1_i64; w[ix] as usize];
-            w2.extend(tmp);
+            let tmp: Vec<i64> = vec![-1_i64; fs[ix] as usize];
+            blocks.extend(tmp);
         }
     }
 
-    w2
+    blocks
 }
 
 fn main() {
     let args = Args::parse();
-    let mut b = parse_blocks(args.input);
+
+    let fs = parse_fill_space(args.input);
+    let mut b1 = expand_blocks(fs.clone());
 
     // sort our list...
-    custom_sort_1(&mut b);
+    custom_sort_1(&mut b1);
 
     let mut sum1 = 0;
-    for (ix, fnum) in b.iter().enumerate() {
+    for (ix, fnum) in b1.iter().enumerate() {
         if *fnum >= 0_i64 {
             sum1 += fnum * ix as i64;
         }
