734e61a25f33ce0668301d61875e76cc64b8272d
standarddeviant <dave.crist@gmail.com>
2024-12-22 12:24:58 -0500

    day22, part1 toy working

---
+++ day22/src/main.rs
@@ -0,0 +1,71 @@
+// use std::ops::BitXor
+// NOTE: To mix a value into the secret number...
+// 1. calculate the bitwise XOR of the given value and the secret number
+// 2. then, the secret number becomes the result of that operation.
+//    - If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.
+fn mix(a: u64, b: u64) -> u64 {
+    a ^ b
+}
+
+// NOTE: pruning is 'modulo X w/ 16777216', but hex(16777216) == 0x1000000, and...
+// modulo X w/ 2**n is equal to X & (2**n - 1)
+fn prune(a: u64) -> u64 {
+    a & (16777216 - 1)
+}
+
+fn mix_prune_basic_check() {
+    let a = 42;
+    let b = 15;
+    println!("mix({}, {}) = {}", a, b, mix(a, b));
+
+    let c = 100000000;
+    println!("prune({}) = {}", c, prune(c));
+}
+
+// In particular, each buyer's secret number evolves into the next secret number in the sequence via the following process:
+//
+// 1a. Calculate the result of multiplying the secret number by 64.
+// 1b. Then, mix this result into the secret number. Finally, prune the secret number.
+//
+// 2a. Calculate the result of dividing the secret number by 32. Round the result down to the nearest integer.
+// 2b. Then, mix this result into the secret number. Finally, prune the secret number.
+//
+// 3a. Calculate the result of multiplying the secret number by 2048.
+// 3b. Then, mix this result into the secret number. Finally, prune the secret number.
+
+// try the naiive approach w/ bit shifts...
+fn evolve(a: u64) -> u64 {
+    let a1 = prune(mix(a, a << 6));
+    let a2 = prune(mix(a1, a1 >> 5));
+    let a3 = prune(mix(a2, a2 << 11));
+    a3
+}
+
+fn evolve_basic_check() {
+    let mut a = 123;
+    for _ix in 0..10 {
+        let olda = a;
+        a = evolve(a);
+        println!("{olda} -> {a}");
+    }
+}
+
+fn main() {
+    mix_prune_basic_check();
+    evolve_basic_check();
+
+    let mut out_vec = vec![];
+    let practice_input = vec![1, 10, 100, 2024];
+    for pi in practice_input {
+        let mut tmp = pi;
+        for _ix in 0..2000 {
+            tmp = evolve(tmp);
+        }
+        out_vec.push(tmp);
+    }
+    let sum1: u64 = out_vec.iter().sum();
+    println!("day22, part1 = {sum1}");
+    // let practice_input: Vec<u64> = vec![
+    //     15887950, 16495136, 527345, 704524, 1553684, 12683156, 11100544, 12249484, 7753432, 5908254,
+    // ];
+}
